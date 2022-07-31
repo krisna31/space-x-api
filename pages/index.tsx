03ae879640de3334bc1371cbaccced4cd399743b
Krisna31Windows <krisnaaaputraaa@gmail.com>
2022-08-01 05:19:51 +0700

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className="flex">
       <Head>ap
         <title>Space X API</title>
         <meta name="description" content="Root Project Of Space X" />
@@ -16,7 +15,7 @@ const Home: NextPage = () => {
         <h1>Welcome To Space X API by Krisna31</h1>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="flex">
       </footer>
     </div>
   )
