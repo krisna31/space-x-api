77d3565a000b6e6258e3ff9ede485bc7148313d0
Krisna31 <81416312+krisna31@users.noreply.github.com>
2022-08-16 17:32:23 +0700

    change from roc-sh-road-lau TO lau-sh-roc-road

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ const Home: NextPage = () => {
       <h1 className='font-bold dark:bg-gradient-to-br dark:from-teal-200 dark:to-lime-200 bg-clip-text text-transparent bg-gradient-to-l from-slate-900 via-slate-800 cursor-default to-slate-900 mb-8 text-lg drop-shadow-lg sm:text-xl md:text-2xl lg:text-3xl text-center'>Welcome To Space X API by Krisna31</h1>
       <ul className='flex flex-wrap flex-col transition-all items-center flex-1 justify-evenly'>
         <li className='cursor-pointer bg-transparent backdrop-blur-xl my-3 text-3xl hover:text-4xl md:text-4xl md:hover:text-5xl lg:text-5xl lg:hover:text-6xl font-bold dark:bg-gradient-to-br dark:from-teal-200 dark:to-lime-200 bg-clip-text text-transparent bg-gradient-to-l from-slate-900 via-slate-800 to-slate-900'>
-          <Link href="/rockets">
-            1. Rockets
+          <Link href="/launches">
+            1. Launches
           </Link >
         </li>
         <li className='cursor-pointer bg-transparent backdrop-blur-xl my-3 text-3xl hover:text-4xl md:text-4xl md:hover:text-5xl lg:text-5xl lg:hover:text-6xl font-bold dark:bg-gradient-to-br dark:from-teal-200 dark:to-lime-200 bg-clip-text text-transparent bg-gradient-to-l from-slate-900 via-slate-800 to-slate-900'>
@@ -17,13 +17,13 @@ const Home: NextPage = () => {
           </Link >
         </li>
         <li className='cursor-pointer bg-transparent backdrop-blur-xl my-3 text-3xl hover:text-4xl md:text-4xl md:hover:text-5xl lg:text-5xl lg:hover:text-6xl font-bold dark:bg-gradient-to-br dark:from-teal-200 dark:to-lime-200 bg-clip-text text-transparent bg-gradient-to-l from-slate-900 via-slate-800 to-slate-900'>
-          <Link href="/roadster">
-            3. Roadster
+          <Link href="/rockets">
+            3. Rockets
           </Link >
         </li>
         <li className='cursor-pointer bg-transparent backdrop-blur-xl my-3 text-3xl hover:text-4xl md:text-4xl md:hover:text-5xl lg:text-5xl lg:hover:text-6xl font-bold dark:bg-gradient-to-br dark:from-teal-200 dark:to-lime-200 bg-clip-text text-transparent bg-gradient-to-l from-slate-900 via-slate-800 to-slate-900'>
-          <Link href="/launches">
-            4. Launches
+          <Link href="/roadster">
+            4. Roadster
           </Link >
         </li>
       </ul>
